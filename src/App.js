09bf7c8a8ccf8e81124deae097056d9395c0e4eb
river-ceanne <snvencer@gmail.com>
2019-05-07 09:46:46 -0700

    counter done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 import React from 'react';
 
-function App() {
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <h1>- Simple Counter App -</h1>
+    </header>
   );
+};
+
+class Content extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  handleCounter = e => {
+    let counter = e.target.value;
+    this.setState({ counter });
+    console.log(this.state);
+  };
+
+  handleClickMinus = e => {
+    e.preventDefault();
+    let counter = this.state.counter - 1;
+    this.setState({ counter });
+  };
+
+  handleClickAdd = e => {
+    e.preventDefault();
+    let counter = this.state.counter + 1;
+    this.setState({ counter });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>{this.state.counter}</h2>
+        <button onClick={this.handleClickMinus}>-</button>
+        <button onClick={this.handleClickAdd}>+</button>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Content />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
